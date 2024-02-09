f83c84fdb8a1ff2507082a6a36826536a1430624
Duvan Mancilla <Duvanred0@gmail.com>
2024-02-09 11:46:18 -0500

    Componente reutulizable boton personalizado

---
+++ src/app/components/CustomButton.jsx
@@ -0,0 +1,14 @@
+'use client'
+const CustomButton = ({title, styles}) => {
+    return(
+        <button
+        type="button"
+        className={styles}
+        onClick={() => {}}
+        >
+            {title}
+        </button>
+    )
+}
+
+export default CustomButton
