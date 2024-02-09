1ba6296c68b85d9149716d3cc8de81d793bf2685
Duvan Mancilla <Duvanred0@gmail.com>
2024-02-09 12:53:45 -0500

    text-balance para mejora visual

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,8 +6,8 @@ export default function Hero (){
         <>
         <div className='hero relative h-[580px] justify-center md:justify-start flex md:items-center'>
             <div className='hero__text text-center md:text-start  md:ml-10 lg:ml-24'>
-                <h1 className='uppercase text-3xl sm:text-4xl md:text-6xl  font-bold text-blue-600'>mustang is <br/> horse power</h1>
-                <p className='text-gray-700 m-1 text-xs md:text-lg md:max-w-[420px]'>Personaliza tu Mustang, añadele poder y roba miradas  con Ford Mustang</p>
+                <h1 className='uppercase text-3xl sm:text-4xl md:text-6xl font-bold text-blue-600'>mustang is <br/> horse power</h1>
+                <p className='text-gray-700 m-2 text-balance text-xs md:text-lg md:max-w-[420px]'>Personaliza tu Mustang, añadele poder y roba miradas  con Ford Mustang</p>
                 <CustomButton
                     title="Cotizar"
                     styles="rounded-lg text-white bg-blue-600 p-2 pl-12 pr-12 mt-5 uppercase"
