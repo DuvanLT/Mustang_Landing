3e97b8d9e3a48451aa064834d80ebbc69083cba4
Duvan Mancilla <Duvanred0@gmail.com>
2024-02-09 11:49:10 -0500

    nueva fuente poppins

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ['200' , '300' , '400' , '500' , '700' ] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
