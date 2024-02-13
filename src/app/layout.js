b761871645f00dcfdfe9735b59495453bb65d4a6
Duvan Mancilla <Duvanred0@gmail.com>
2024-02-13 13:31:29 -0500

    mensaje modificado

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    /*Esta hecha con React + Next.js que se me olvidaba decirlo (Mustang >> Camaro)*/ 
+    /*Esta hecha con React + Next.js que se me olvidaba decirlo (Mustang >> Camaro)
+    Si no sabes diseño aprende figma aqui el diseño figma de esta web https://www.figma.com/file/kYqKDZ3NfeiwCaCCSq4QGB/Mustang?type=design&mode=design&t=YuuhfKmLAfDga0Hq-1
+    */
     <html lang="es" class="scroll-smooth">
       <body className={poppins.className}>{children}</body>
     </html>
